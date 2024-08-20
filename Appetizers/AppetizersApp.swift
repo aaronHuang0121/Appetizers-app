0bcbc5e3707d713d0a821a2bc3574f603b32f1c1
Aaron Huang <aaron.huang0121@gmail.com>
2024-08-20 14:32:11 +0800

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Aaron on 2024/8/20.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
