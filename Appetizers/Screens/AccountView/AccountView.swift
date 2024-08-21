b770376d203ca54c969597925289da91caad556b
Aaron Huang <aaron.huang0121@gmail.com>
2024-08-21 17:28:25 +0800

    feat: added focus state for text fields

--- Appetizers/Screens/AccountView/AccountView.swift
+++ Appetizers/Screens/AccountView/AccountView.swift
@@ -9,18 +9,32 @@ import SwiftUI
 
 struct AccountView: View {
     @StateObject private var viewModel = AccountViewModel()
+    @FocusState private var focusState: FormTextField?
 
     var body: some View {
         NavigationView {
             Form {
                 Section("Personal info") {
                     TextField("First Name", text: $viewModel.user.firstName)
+                        .focused($focusState, equals: .firstName)
+                        .onSubmit { focusState = .lastName }
+                        .submitLabel(.next)
+
                     TextField("Last Name", text: $viewModel.user.lastName)
+                        .focused($focusState, equals: .lastName)
+                        .onSubmit { focusState = .email }
+                        .submitLabel(.next)
+
                     TextField("Email", text: $viewModel.user.email)
+                        .focused($focusState, equals: .email)
+                        .onSubmit { focusState = nil }
+                        .submitLabel(.continue)
                         .keyboardType(.emailAddress)
                         .textInputAutocapitalization(.never)
                         .autocorrectionDisabled()
+
                     DatePicker("Birthday", selection: $viewModel.user.birth, displayedComponents: .date)
+
                     Button(
                         action: viewModel.saveChanges,
                         label: {
@@ -40,7 +54,20 @@ struct AccountView: View {
             .alert(isPresented: $viewModel.showAlert, error: viewModel.formMessage) {
                 Button("OK", role: .none, action: {})
             }
+            .toolbar {
+                ToolbarItem(placement: .keyboard) {
+                    Button("Dismiss", action: { focusState = nil })
+                }
+            }
         }
+    }
+}
+
+extension AccountView {
+    enum FormTextField: Hashable {
+        case firstName
+        case lastName
+        case email
     }
 }
 
